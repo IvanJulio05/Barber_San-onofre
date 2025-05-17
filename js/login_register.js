9fded79425e394873568c5ad2c9a552fe0138619
ivan <ivancamilojulioguerrero9@gmail.com>
2025-05-17 18:20:58 -0500

    se agrego la logica del login, y una parte del perfil del barbero admin

--- js/login_register.js
+++ js/login_register.js
@@ -15,6 +15,7 @@ createAccountButton.addEventListener("click", () => {
         valid_password.placeholder = "Confirmar Contraseña";
         valid_password.type = "password";
         valid_password.required = true;
+        valid_password.id = "valid_password";
         input_name.placeholder = "Nombre";
         input_name.type = "text";
         input_name.required = true;
@@ -24,9 +25,122 @@ createAccountButton.addEventListener("click", () => {
     else{
         button.textContent = "Iniciar Sesion";
         createAccountButton.textContent = "Crear Cuenta";
+
         const input_name = document.querySelector("input[type='text']");
+
+        const valid_password = document.getElementById("valid_password");
+
         conteinerInput.removeChild(input_name);
+        conteinerInput.removeChild(valid_password);
     }
 
   
 });
+
+
+//enviar informacion al servidor
+const btLogin = document.querySelector("#login");
+
+btLogin.addEventListener("click",()=>{
+
+    const password = document.querySelector("input[type='password']");
+    const email = document.querySelector("input[type='email']");
+
+    if(button.textContent == "Iniciar Sesion"){
+        if(password.value == "" || email.value == ""){
+            alert("Por favor completa todos los campos");
+        }
+        else{
+
+            const user={
+                email: email.value,
+                password: password.value
+            }
+            
+            fetch("http://localhost:8080/barber/login", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify(user)
+            })
+            .then(response => {
+
+                if (!response.ok) {
+                    throw new Error('Network response was not ok ' + response.statusText);
+                }
+
+                let token = response.headers.get("Authorization");
+                localStorage.setItem("token",token);
+
+                if(response.status == 401){
+                    alert("Correo o contraseña incorrecta");
+                }
+
+
+                return response.json(); // Parsear la respuesta JSON
+            })
+            .then(data => {           
+                console.log('Success:', JSON.stringify(data));
+                // Aquí puedes manejar la respuesta de la API        
+                
+                localStorage.setItem("user",JSON.stringify(data));
+                localStorage.setItem("showForm","false");
+                window.location.replace('../html/perfil_admin.html');
+            })
+        }
+    }
+    else{
+        const name = document.querySelector("input[type='text']");
+        const valid_password = document.getElementById("valid_password");
+        if(email.value == "" || password.value == "" || name.value == "" || valid_password.value == ""){
+            alert("Por favor completa todos los campos");
+        }
+        else if(password.value == valid_password.value){
+
+            const user={
+                email: email.value,
+                password: password.value,
+                name: name.value
+            }
+            
+            fetch("http://localhost:8080/barber/register", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify(user)
+            })
+            .then(response => {
+
+                if (!response.ok) {
+                    throw new Error('Network response was not ok ' + response.statusText);
+                }
+
+                let token = response.headers.get("Authorization");
+                localStorage.setItem("token",token);
+
+                if(response.status == 409){
+                    alert("El correo ya esta registrado");
+                }
+
+
+                return response.json(); // Parsear la respuesta JSON
+            })
+            .then(data => {           
+                // Aquí puedes manejar la respuesta de la API        
+                
+                localStorage.setItem("user",JSON.stringify(data));
+                localStorage.setItem("showForm","true");
+                
+                window.location.replace('../html/perfil_admin.html');
+            })            
+
+
+        }
+        else{
+            alert("Las contraseñas no coinciden");
+        }
+    }
+
+})
--- js/perfil_admin.js
+++ js/perfil_admin.js
@@ -4,8 +4,107 @@ const btModi = document.getElementById("bt_modi");
 const close = document.getElementById("close");
 
 btModi.addEventListener("click", function() {
+
+    const user=JSON.parse(localStorage.getItem("user"));
+    const input_name = document.getElementById("name");
+    const input_price = document.getElementById("price");
+    const input_nighborhood = document.getElementById("barrio");
+    const input_address = document.getElementById("direccion");
+    input_name.value= user.name;
+    input_price.value= user.price;
+    input_nighborhood.value= user.neighborhood;
+    input_address.value= user.address;
+    //rellenar campos automaticamente
+
     dialogo.showModal();
 });
 close.addEventListener("click", function() {
-    dialogo.close();
+    
+    const input_name = document.getElementById("name");
+    const input_price = document.getElementById("price");
+    const input_nighborhood = document.getElementById("barrio");
+    const input_address = document.getElementById("direccion");
+    let token= "Bearer " + localStorage.getItem("token");
+    if(input_name.value == "" || input_price.value == "" || input_nighborhood.value == ""){
+        alert("Por favor completa todos los campos");
+
+    }
+    else{
+
+        const user=JSON.parse(localStorage.getItem("user"));
+        const userUpdate={
+            name: input_name.value,
+            email: user.email,
+            price: input_price.value,
+            neighborhood: input_nighborhood.value,
+            address: input_address.value
+        }
+        //enviar informacion al servidor
+
+        fetch("http://localhost:8080/barber/update/"+user.id, {
+            method: "PUT",
+            headers: {
+                "Content-Type": "application/json",
+                'Authorization': token
+            },
+            body: JSON.stringify(userUpdate)
+        })
+        .then(response => {
+
+            if (!response.ok) {
+                throw new Error('Network response was not ok ' + response.statusText);
+            }
+
+
+            return response.json(); // Parsear la respuesta JSON
+        })
+        .then(data => {           
+            // Aquí puedes manejar la respuesta de la API        
+            
+            localStorage.setItem("user",JSON.stringify(data));
+            
+            location.reload();
+        })   
+
+
+
+        dialogo.close();
+    }
+    
+});
+
+
+document.addEventListener("DOMContentLoaded", function() {
+
+    const user=JSON.parse(localStorage.getItem("user"));
+    if(localStorage.getItem("showForm") == "true"){
+
+        //rellenar campos automaticamente
+        const input_name_dialog = document.getElementById("name");
+        input_name_dialog.value= user.name;
+        dialogo.showModal();
+        localStorage.setItem("showForm","false");
+    }
+    else{
+        const user_all_info=JSON.parse(localStorage.getItem("user"));
+
+        let txt_name = document.getElementById("txt_name");
+        let txt_price = document.getElementById("txt_price");
+        let txt_score = document.getElementById("txt_score");
+        let txt_address = document.getElementById("txt_address");
+        let txt_number_clients = document.getElementById("txt_number_clients");
+
+        txt_name.textContent=user_all_info.name;
+        txt_price.textContent=user_all_info.price;
+        txt_score.textContent=user_all_info.score;
+        txt_address.textContent=user_all_info.address+", "+user_all_info.neighborhood;
+        //txt_number_clients.textContent=user_all_info.clients.length;
+        console.log(user_all_info);
+    }
+
+
+    //rellenando informacion de la pagina
+
+
+
 });
